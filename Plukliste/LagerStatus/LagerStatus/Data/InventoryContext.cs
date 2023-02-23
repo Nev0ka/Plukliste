75a634c7e7afbf5eb6688af8d4db6d87124186b1
Nev0ka <nicolai@osberg.info>
2023-02-23 09:12:41 +0100

    yolo

---
+++ Plukliste/LagerStatus/LagerStatus/Data/InventoryContext.cs
@@ -0,0 +1,24 @@
+﻿using LagerStatus.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LagerStatus.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
